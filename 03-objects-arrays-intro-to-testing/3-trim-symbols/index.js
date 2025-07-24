e1722964ab4e2325859aa948c836093f7f0124fa
Вадим Рулёв <rulev.v@promail366.com>
2025-07-24 15:59:27 +0300

    Урок 3. trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,32 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
 
+  if (size === 0) {
+    return '';
+  }
+
+  let result = '';
+  let count = 0;
+  let prevChar = '';
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+
+    if (char === prevChar) {
+      if (count < size) {
+        result += char;
+        count++;
+      }
+    } else {
+      result += char;
+      count = 1;
+      prevChar = char;
+    }
+  }
+
+  return result;
 }
